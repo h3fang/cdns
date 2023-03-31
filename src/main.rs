e75cc4b79fb9fc799515381b0dfba56a2fa4e063
Fang He <hf.enigma@gmail.com>
2023-03-31 10:49:22 +0800

    add context to errors

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod server;
 
 use std::{net::SocketAddr, sync::Arc};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use log::{error, info, warn};
 use tokio::net::UdpSocket;
 use trust_dns_proto::op;
@@ -15,7 +15,9 @@ use resolver::Resolver;
 
 async fn respond(msg: &op::Message, sock: &UdpSocket, addr: &SocketAddr) -> Result<()> {
     let bytes = msg.to_vec()?;
-    sock.send_to(&bytes, addr).await?;
+    sock.send_to(&bytes, addr)
+        .await
+        .context("Failed to send DNS response packet.")?;
     for ans in msg.answers() {
         info!("{ans}");
     }
@@ -23,17 +25,16 @@ async fn respond(msg: &op::Message, sock: &UdpSocket, addr: &SocketAddr) -> Resu
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     let env = env_logger::Env::default().default_filter_or("info");
     env_logger::Builder::from_env(env)
         .format_timestamp_micros()
         .init();
 
     let config = match std::env::args().nth(1) {
-        Some(path) => match Config::from_file(&path) {
-            Ok(cfg) => cfg,
-            Err(e) => panic!("Failed to parse config file: {path}, error: {e}"),
-        },
+        Some(path) => {
+            Config::from_file(&path).context(format!("Failed to parse config file: {path}"))?
+        }
         None => {
             warn!("Using default configuration.");
             Config::default()
@@ -42,13 +43,20 @@ async fn main() -> std::io::Result<()> {
 
     let resolver = Arc::new(Resolver::new(config, 2048));
 
-    let sock = Arc::new(UdpSocket::bind("127.0.0.1:53").await?);
+    let sock = Arc::new(
+        UdpSocket::bind("127.0.0.1:53")
+            .await
+            .context("Failed to create and bind socket 127.0.0.1:53")?,
+    );
 
     // maximum size of the DNS message, from https://datatracker.ietf.org/doc/html/rfc8484#section-6
     let mut buf = vec![0u8; 65535];
 
     loop {
-        let (len, addr) = sock.recv_from(&mut buf).await?;
+        let (len, addr) = sock
+            .recv_from(&mut buf)
+            .await
+            .context("Failed to read data from socket.")?;
         let bytes = buf[..len].to_vec();
 
         let sock = sock.clone();
